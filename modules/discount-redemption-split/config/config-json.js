76d06623363965805f15ca2113ed5e15ba52a6a5
tienar <62941100+tienar@users.noreply.github.com>
2021-02-04 12:59:02 -0500

    Update config-json.js

--- modules/discount-redemption-split/config/config-json.js
+++ modules/discount-redemption-split/config/config-json.js
@@ -28,6 +28,7 @@ module.exports = function configJSON(req) {
       }
     },
     configurationArguments: {
+      "applicationExtensionKey": "bbff74f0-847e-4527-8dcb-e2ac5c9611f1",
       save: {
         url: `https://sfmc-tienar.herokuapp.com/modules/discount-redemption-split/save`
       },
